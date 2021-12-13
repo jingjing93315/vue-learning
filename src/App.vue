435dc46a5186d81818eb4a75bb6035eedf909b5f
gaojing <gaojing@tyb365.com>
2021-12-13 09:57:32 +0800

    插槽案例

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,84 @@
 <template>
   <div class="container">
-    <div class="todo-container">
+    <!-- <div class="todo-container">
       <div class="todo-wrap">
         <MyHeader @addTodo="addTodo" />
         <MyList :todos="todos" />
         <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo" />
       </div>
-    </div>
-     <div class="github-container">
+    </div> -->
+    <!-- <div class="github-container">
       <h3>github案例</h3>
       <Search/>
       <hr/>
       <List />
+    </div> -->
+    <div class="category-container">
+      <Category title="美食">
+        <img class="img" slot="center" :src="imgUrl" alt="">
+        <div class="foot" slot="footer">
+          <a href="https://baidu.com">更多美食</a>
+        </div>
+      </Category>
+      <Category title="游戏">
+        <ul>
+          <li v-for="(item, index) in games" :key="index">{{item}}</li>
+        </ul>
+        <div class="foot" slot="footer">
+          <a href="https://baidu.com">单机游戏</a>
+          <a href="https://baidu.com">网络游戏</a>
+        </div>
+      </Category>
+      <Category title="电影">
+        <video slot="center" controls class="video" src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"></video>
+        <template v-slot:footer>
+          <div class="foot">
+            <a href="https://baidu.com">经典</a>
+            <a href="https://baidu.com">热门</a>
+            <a href="https://baidu.com">推荐</a>
+          </div>
+          <h4 class="center">welcome</h4>
+        </template>
+      </Category>
     </div>
   </div>
 </template>
 <script>
-import axios from 'axios'
-import pubsub from 'pubsub-js'
-import MyHeader from './components/MyHeader'
-import MyFooter from './components/MyFooter'
-import MyList from './components/MyList'
-import Search from './components/github/Search.vue'
-import List from './components/github/List.vue'
+import Category from "./components/Category.vue"
+// import axios from 'axios'
+// import pubsub from 'pubsub-js'
+// import MyHeader from './components/MyHeader'
+// import MyFooter from './components/MyFooter'
+// import MyList from './components/MyList'
+// import Search from './components/github/Search.vue'
+// import List from './components/github/List.vue'
 
 export default {
   name: 'App', //汇总所有子组件
   components: {
-    MyHeader,
-    MyFooter,
-    MyList,
-    Search,
-    List
+    Category
   },
   data() {
     return {
-      todos: JSON.parse(localStorage.getItem('todos')) ?? []
+      // todos: JSON.parse(localStorage.getItem('todos')) ?? []
+      foods: [
+        '火锅', '烧烤', '小龙虾', '牛排'
+      ],
+      games: [
+        '王者荣耀', '超级玛丽', '穿越火线'
+      ],
+      films: [
+        '《教父》', "《你好，李焕英》", "《菊次郎的夏天》"
+      ],
+      imgUrl: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/180/h/180',
+      videoUrl: ''
     }
   },
   mounted() {
-    this.$bus.$on('checkTodo', this.checkTodo)
-    this.$bus.$on('updateTodo', this.updateTodo)
+    // this.$bus.$on('checkTodo', this.checkTodo)
+    // this.$bus.$on('updateTodo', this.updateTodo)
     // this.$bus.$on('deleteTodo', this.deleteTodo)
-    this.pubId = pubsub.subscribe('deleteTodo', this.deleteTodo)
+    // this.pubId = pubsub.subscribe('deleteTodo', this.deleteTodo)
   },
   methods: {
     // 添加一个todo
@@ -143,7 +179,8 @@ body {
   outline: none;
 }
 
-.todo-container,.github-container {
+.todo-container,
+.github-container {
   width: 600px;
   margin: 0 auto;
 }
@@ -154,4 +191,20 @@ body {
   border-radius: 5px;
 }
 
+/* category UI */
+.category-container {
+  display: flex;
+  justify-content: space-around;
+}
+.video,
+.img {
+  width: 100%;
+}
+.foot {
+  display: flex;
+  justify-content: space-around;
+}
+.center {
+  text-align: center;
+}
 </style>
---
+++ src/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <slot name="center" />
+    <slot name="footer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.category {
+  width: 200px;
+  height: 300px;
+  background: skyblue;
+}
+h3 {
+  text-align: center;
+  background: orange;
+}
+</style>
